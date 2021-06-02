6e8809cb6f182c0b415a4befbec483247c01ebbc
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-02 18:15:48 -0400

    ran migrations for review and track tables

--- earhustlerz-backend/db/schema.rb
+++ earhustlerz-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_091425) do
+ActiveRecord::Schema.define(version: 2021_05_18_102550) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,28 @@ ActiveRecord::Schema.define(version: 2021_05_18_091425) do
     t.index ["user_id"], name: "index_albums_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.integer "user_id", null: false
+    t.integer "album_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_reviews_on_album_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "title"
+    t.string "artist"
+    t.time "duration"
+    t.integer "album_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_tracks_on_album_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -32,4 +54,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_091425) do
   end
 
   add_foreign_key "albums", "users"
+  add_foreign_key "reviews", "albums"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "tracks", "albums"
 end
