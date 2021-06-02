96e0347dbbe54aadc5d897b476b8f53845e55e6e
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-01 22:11:39 -0400

    added namespacing to url/ ran user migration/ seeded in 1 user/

--- earhustlerz-backend/config/routes.rb
+++ earhustlerz-backend/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+  # earhustlerz.com/api/v1
+ namespace :api do
+   namespace :v1 do
+    resources :users
+   end
+ end 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
