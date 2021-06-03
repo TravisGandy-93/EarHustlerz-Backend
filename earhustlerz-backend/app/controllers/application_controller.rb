040a8e71f6af75f060328e75795a8df186f04297
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-02 21:49:29 -0400

    created some mock login info in app controller

--- earhustlerz-backend/app/controllers/application_controller.rb
+++ earhustlerz-backend/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    helpers do
+        def current_user
+            #mock log in info
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
