aededeeaceca700d60a5ba6aa57385f3a9373ff4
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-08 02:07:37 -0400

    built get current user logic for when component mounts on frontend

--- earhustlerz-backend/app/controllers/application_controller.rb
+++ earhustlerz-backend/app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-    helpers do
+
         def current_user
             #mock log in info
-            User.find(session[:user_id])
+            User.find_by(id: session[:user_id])
         end
 
         def logged_in?
             !!current_user
         end
-    end
+    
 end
