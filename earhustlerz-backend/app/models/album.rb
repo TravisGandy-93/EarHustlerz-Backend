5b3d61887451697de5de43c831deee4e50636c1a
TravisGandy-93 <61717834+TravisGandy-93@users.noreply.github.com>
2021-06-29 12:50:42 -0400

    ensuring reviews get deleted with album

--- earhustlerz-backend/app/models/album.rb
+++ earhustlerz-backend/app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ApplicationRecord
   belongs_to :user
   has_many :tracks
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :title, :cover, :artist, presence: true
 end
